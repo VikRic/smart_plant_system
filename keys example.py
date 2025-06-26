d3e34d1742b7217253f55d830755031593f16ef3
VikRic <vr222im@student.lnu.se>
2025-06-26 22:05:53 +0200

    Forgot to add Port in example

--- keys example.py
+++ keys example.py
@@ -5,7 +5,8 @@ WIFI_SSID = "WIFI_SSID" # Enter your WIFI SSID
 WIFI_PASS = "WIFI_PASSWORD" # Enter your WIFI PASSWORD
 
 # MQTT keys
-MQTT_BROKER = "serveradress:port" # Change to your server  
+MQTT_BROKER = "serveradress" # Change to your server  
+MQTT_PORT = 1883                  # Default port is 1883  
 MQTT_USER = "admin"               # Change this
 MQTT_PASS = "password"            # Change this
 MQTT_TOPIC = "TopicName"          # any unique name
